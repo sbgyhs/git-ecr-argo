c8b383a298ce164b24b984cea9f391587cbd32b4
sbgyhs <hoseop4018@naver.com>
2024-08-01 20:41:38 +0900

    test

--- Collection_of_Dockerfiles/server/prisma/migrations/20240716082706_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AddForeignKey
-ALTER TABLE `order` ADD CONSTRAINT `order_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `order` ADD CONSTRAINT `order_bookId_fkey` FOREIGN KEY (`bookId`) REFERENCES `book`(`bookid`) ON DELETE RESTRICT ON UPDATE CASCADE;
