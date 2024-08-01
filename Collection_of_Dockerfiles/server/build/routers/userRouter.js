ea9702356818146f9bd169f5bc0f98a0ae15e293
sbgyhs <hoseop4018@naver.com>
2024-08-01 15:09:13 +0900

    ready

--- Collection_of_Dockerfiles/server/build/routers/userRouter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
-const userController_1 = require("../controllers/userController");
-const router = express.Router();
-router.post('/', userController_1.createUser);
-module.exports = router;
--- Collection_of_Dockerfiles/server/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-
-app.listen(8080, function () {
-  console.log('listening on 8080')
-}); 
